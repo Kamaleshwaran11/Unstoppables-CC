c3bcfabd21f6ed4020ca1400ca67bea039817df4
kamaleshwaran11 <shankarkamal52@gmail.com>
2025-06-10 19:14:32 +0530

    New Changes

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.getenv('MYSQL_USER', 'root')}:{os.getenv('MYSQL_PASSWORD', 'Sk@3117')}@"
-        f"{os.getenv('MYSQL_HOST', 'localhost')}/{os.getenv('MYSQL_DB', 'unstoppablescc')}"
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
